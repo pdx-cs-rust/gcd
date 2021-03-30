6a5f47279878ad985d1257a78549e90de5f43cbb
Bart Massey <bart.massey@gmail.com>
2021-03-30 13:39:10 -0700

    cleaned up some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
+/// Compute the greatest common denominator of `m` and `n`.
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
         if m < n {
-            let t = m;
-            m = n;
-            n = t;
+            std::mem::swap(&mut m, &mut n);
         }
-        m = m % n;
+        m %= n;
     }
     n
 }
@@ -20,7 +19,6 @@ fn test_gcd() {
                3 * 11);
 }
 
-use std::io::Write;
 use std::str::FromStr;
 
 fn main() {
@@ -31,8 +29,8 @@ fn main() {
                      .expect("error parsing argument"));
     }
 
-    if numbers.len() == 0 {
-        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+    if numbers.is_empty() {
+        eprintln!("Usage: gcd NUMBER ...");
         std::process::exit(1);
     }
 
@@ -41,6 +39,8 @@ fn main() {
         d = gcd(d, *m);
     }
 
-    println!("The greatest common divisor of {:?} is {}",
-             numbers, d);
+    let numprint: Vec<String> =
+        numbers.iter().map(|n| n.to_string()).collect();
+    println!("The greatest common divisor of {} is {}",
+             numprint.join(","), d);
 }
